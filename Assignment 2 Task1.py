7c82c3a7a069b255bb331bddf71f90209275f8ae
SauravBonik <sauravbonik01@gmail.com>
2025-05-31 19:10:12 +0530

    Add files via upload

---
+++ Assignment 2 Task1.py
@@ -0,0 +1,6 @@
+
+x = int(input("Enter a number: "))
+if x % 2 == 0:
+    print("\n", x, "is an even number")
+else:
+    print("\n", x, "is an odd number")
---
+++ Assignment 2 Task2.py
@@ -0,0 +1,22 @@
+
+total = 0
+for x in range(1, 51):  # Loop from 1 to 50 (inclusive)
+    total += x
+print("The sum of numbers from 1 to 50 is", "\n", total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
